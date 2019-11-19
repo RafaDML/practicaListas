3315741a49dda0698d3a2c59d248c818ec53a615
RafaDML <rafaelmedinalozano@gmail.com>
2019-11-19 15:30:36 -0700

    loginrevisado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+  
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -8,18 +9,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import {HttpClientModule} from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule], 
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot()
+  ],
   providers: [
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-    
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides, NavController } from '@ionic/angular';
+import { BuscarService } from '../service/buscar.service';
+import { NgForm } from '@angular/forms';
+import { Storage } from '@ionic/storage';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss']
+})
+export class LoginPage implements OnInit {
+  @ViewChild('slidePrincipal', { static: false }) slides: IonSlides;
+  seleccion = true;
+  loginUser = {
+    usuario: 'prueba',
+    pass: '123',
+    tipoMov: 'login'
+  };
+
+  DatosUsuarios: any;
+  constructor(
+    private serv: BuscarService,
+    private storage: Storage,
+    private navCtrl: NavController
+  ) {}
+
+  ngOnInit() {}
+
+  ionViewWillEnter() {
+    this.slides.lockSwipes(true);
+  }
+
+  async login(fLogin: NgForm) {
+    /* console.log(fLogin.valid); */
+    if (fLogin.invalid) {
+      return;
+    }
+
+    this.DatosUsuarios = await this.serv.postData(this.loginUser);
+    console.log(this.DatosUsuarios);
+
+    if (this.DatosUsuarios.success) {
+      /* console.log('entrar');*/
+      // navegar al tabs
+      this.navCtrl.navigateRoot('home', { animated: true });
+      this.storage.set('dataUsarios', this.DatosUsuarios.result);
+    } else {
+      /* console.log('no entrar'); */
+    }
+  }
+
+  async registro(fRegistro: NgForm) {
+    if (fRegistro.invalid) {
+      return;
+    }
+  }
+
+  mostrarRegistro() {
+    this.slides.lockSwipes(false);
+    this.seleccion = false;
+    this.slides.slideTo(1);
+    this.slides.lockSwipes(true);
+  }
+
+  mostrarLogin() {
+    this.slides.lockSwipes(false);
+    this.seleccion = true;
+    this.slides.slideTo(0);
+    this.slides.lockSwipes(true);
+  }
+}
